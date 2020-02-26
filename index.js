434cf442bb7ed750554771ee4c775dfe31d5a389
j721 <juliadvu@gmail.com>
2020-02-25 20:02:48 -0600

    Updates to counterMakerWithLimit

--- index.js
+++ index.js
@@ -294,7 +294,7 @@ function counterMaker() {
 */
 function counterMakerWithLimit(maxValue) {
   let count = -1;
-  return function(){
+  return function counter(){
     if(count === maxValue){
     count = -1;
     return ++count;
